6b9bfc58d2a20f5c4704eb0a771739f56d3c86c7
Maico <maicoloncomilla@gmail.com>
2021-01-18 20:18:30 -0300

    Cambiado la seccion de MySkills

--- client/src/components/mySkill/MySkill.js
+++ client/src/components/mySkill/MySkill.js
@@ -3,6 +3,13 @@ import sContainer from '../styles/container.module.css';
 import sText from '../styles/text.module.css';
 import { arrayFront, arrayBack } from '../utils/array';
 import { useSelector } from 'react-redux';
+import redux from '../../images/redux.png';
+import react from '../../images/React.svg';
+import css from '../../images/css.svg';
+import express from '../../images/express.png';
+import html from '../../images/html.svg';
+import javascript from '../../images/javascript.png';
+import node from '../../images/node.svg';
 
 export default function MySkill() {
 
@@ -14,7 +21,14 @@ export default function MySkill() {
             <div className={sContainer.containerSectionFrontBack}>
                 <div className={sContainer.containerMainProgress}>
                     <h2 className={sText.textMySkillFrontBack}>Front End</h2>
-                    {arrayFront.map((el, index) =>
+                    <div className={sContainer.imgIcons}>
+                        <img src={react}/>
+                        <img src={redux}/>
+                        <img src={css}/>
+                        <img src={html}/>
+                        <img src={javascript}/>
+                    </div>
+                    {/* {arrayFront.map((el, index) =>
                         <div className={sContainer.containerProgress} key={index}>
                             <div>
                                 <label className={sText.labelTitleMySkill}>{el.title}</label>
@@ -22,12 +36,17 @@ export default function MySkill() {
                             </div>
                             <progress max={100} value={el.value} />
                         </div>
-                    )}
+                    )} */}
+                    
 
                 </div>
                 <div className={sContainer.containerMainProgress}>
                     <h2 className={sText.textMySkillFrontBack}>Back End</h2>
-                    {arrayBack.map((el, index) =>
+                    <div className={sContainer.imgIconsBack}>
+                        <img src={node}/>
+                        <img src={express}/>
+                    </div>
+                    {/* {arrayBack.map((el, index) =>
                         <div className={sContainer.containerProgress} key={index}>
                             <div>
                                 <label className={sText.labelTitleMySkill}>{el.title}</label>
@@ -35,7 +54,7 @@ export default function MySkill() {
                             </div>
                             <progress max={100} value={el.value} />
                         </div>
-                    )}
+                    )} */}
                 </div>
             </div>
         </section>
